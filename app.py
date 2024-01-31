fc05349133e05954c0f7f6f16b297ad5c05f2b3d
ametial <99119220+ametial@users.noreply.github.com>
2024-01-31 12:28:19 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/") # the route, or the empty route that lands us to the homepage
-def hello_world():
-    return "Hello, World!"
+def hello_template():
+    return render_template('home.html')
 
 print(__name__)
 if __name__ == "__main__":
