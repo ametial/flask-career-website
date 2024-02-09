5e445b1af9b8805820f5f183e352cee5e79abe31
ametial <99119220+ametial@users.noreply.github.com>
2024-02-09 08:13:53 +0000

    added slash to the /api/jobs

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
+
 app = Flask(__name__)
 
 
@@ -18,7 +19,7 @@ def list_jobs():
 def show_job(id):
   job = load_job_from_db(id)
   if not job:
-    return "Not Found", 404
+    return "Not Found", 404gm
     
   return render_template('jobpage.html', job=job)
 
