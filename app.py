1777719a355528cfd11b3a180184d9c3deb5e0c9
ametial <99119220+ametial@users.noreply.github.com>
2024-02-09 08:16:04 +0000

    fixed syntax error on app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def list_jobs():
 def show_job(id):
   job = load_job_from_db(id)
   if not job:
-    return "Not Found", 404gm
+    return "Not Found", 404
     
   return render_template('jobpage.html', job=job)
 
